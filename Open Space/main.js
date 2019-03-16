4c0adb943bb894bf8052b855d4803a9661208daf
Avery <htukappa@blah.com>
2019-03-16 15:57:06 -0700

    Got basic hover sound effects working with bad placeholder sound assets

--- Open Space/main.js
+++ Open Space/main.js
@@ -1,6 +1,40 @@
-var audio1 = document.getElementById("beep-one");
+// var audio1 = document.getElementById("beep-one");
+
+// function playAudio() {
+// 	console.log(audio1);
+//   audio1.play();
+// }
+
+
+var media = document.getElementById("beep-one");
+
 
 function playAudio() {
-	console.log(audio1);
-  audio1.play();
-}
+	const playPromise = media.play();
+	if (playPromise !== undefined) {
+    playPromise.then(_ => {
+      // Automatic playback started!
+			// Show playing UI.
+			console.log(media)
+    })
+    .catch(error => {
+      // Auto-play was prevented
+      // Show paused UI.
+    });
+  }
+// }
+
+// function pauseAudio() {
+// 	const playPromise = media.pause();
+// 	if (playPromise !== undefined) {
+//     playPromise.then(_ => {
+//       // Automatic playback started!
+// 			// Show playing UI.
+// 			console.log(media)
+//     })
+//     .catch(error => {
+//       // Auto-play was prevented
+//       // Show paused UI.
+//     });
+//   }
+// }
